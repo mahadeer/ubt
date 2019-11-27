cdec78eb72ad8ca62a2e56258c4ffeb81e2125dc
mahadeer13 <mahadeer2010@gmail.com>
2019-11-27 16:44:48 +0530

    Content Layou Design Completed

--- src/layout/ContentLayout.jsx
+++ src/layout/ContentLayout.jsx
@@ -1,32 +1,77 @@
-import React from 'react'
+import React from 'react';
+import XMLViewer from 'react-xml-viewer';
+import "./ContentLayout.css";
 
-export const ContentLayout = ({ children, kind, ...rest }) => (
-    <div id="content">
-        <div className="header">
-            <h1>{rest['title']}</h1>
+export const ContentLayout = (props) => {
+    const properties = JSON.parse(props.properties);
+    const examples = JSON.parse(props.examples);
+    const xmlCustomTheme = {
+        "tagColor": "#4CAF50"
+    };
+    const requires = JSON.parse(props.requires);
+    const related = JSON.parse(props.related);
+    return (<div id="content">
+        <h1>{props['title']}</h1>
+        <p dangerouslySetInnerHTML={{ __html: props.description }}></p>
+        <div className="properties">
+            <h3>Properties</h3>
+            <table className="">
+                <tbody>
+                    <tr className="thead uk-background-secondary">
+                        <th>Attribute</th>
+                        <th>Description</th>
+                        <th>Options</th>
+                        <th>Required</th>
+                    </tr>
+                    {
+                        properties.map(prop => (
+                            <tr key={prop.key}>
+                                <td>{prop.key}</td>
+                                <td dangerouslySetInnerHTML={{ __html: prop.description }}></td>
+                                <td>{prop.possibleValues.join(", ")}<br />Default is {prop.default}</td>
+                                <td>{prop.required}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
-        {children}
-    </div>
-);
-
-var schema = {
-    title: "",
-    description: "", // HTML (maybe)
-    syntax: "",
-    properties: [
-        {
-            key: "",
-            description: "",
-            possibleValues: [],
-            default: "",
-            isMandatory: false
+        <div className="examples">
+            <h3>Examples</h3>
+            {
+                examples.map((example, idx) => (
+                    <div className="example" key={idx}>
+                        <p>{example.description}</p>
+                        <div className="example-code"><XMLViewer xml={example.content} theme={xmlCustomTheme}></XMLViewer></div>
+                        {example.note != "" ? <p className="example-note">Note: {example.note}</p> : null}
+                        {idx != examples.length - 1 ? <hr /> : null}
+                    </div>
+                ))
+            }
+        </div>
+        {requires.length > 0 ?
+            <div className="requires">
+                <h3>Requires</h3>
+                <ul className="require-list">
+                    {
+                        requires.map((req, idx) => (
+                            <li><a href={req.route}>{req.text}</a></li>
+                        ))
+                    }
+                </ul>
+            </div> : null
         }
-    ],
-    examples: [
-        {
-            description: "",
-            content: ""
+        {related.length > 0 ?
+            <div className="related">
+                <h3>References</h3>
+                <ul className="reference-list">
+                    {
+                        related.map((rel, idx) => (
+                            <li><a href={rel.route}>{rel.text}</a></li>
+                        ))
+                    }
+                </ul>
+            </div> : null
         }
-    ],
-    requires: []
-}
+    </div>);
+};
