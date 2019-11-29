6216696d96ec70fa608c6cac51bb4bd2951aa202
mahadeer13 <mahadeer2010@gmail.com>
2019-11-29 17:18:46 +0530

    Introduction completed

--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,5 @@ export default {
   base: '/ubs/',
   title: "Unified Build System",
   description: "Rust based Build Tool for Everything and Everyone",
-  menu: ['Introduction', 'Background and Concepts', 'Tutorials', 'Reference', 'Usage Policy', 'Contribute']
+  menu: ['Introduction', 'Background and Concepts', 'Roadmap', 'Tutorials', 'Reference', 'Usage Policy', 'Contribute']
 }
