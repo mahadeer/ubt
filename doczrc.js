968b36c8f46bf067d64a47780f33410d1287ad97
Mahadeer Mohamed <mahadeer2010@gmail.com>
2019-12-03 03:42:26 +0530

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,5 @@
 export default {
-  base: '/ubt/',
+  base: '/ubt',
   title: "Unified Build Tool",
   description: "Rust based Build Tool for Everything and Everyone",
   menu: ['Introduction', 'Background and Concepts', 'Roadmap', 'Tutorials', 'Reference', 'Usage Policy', 'Contribute']
