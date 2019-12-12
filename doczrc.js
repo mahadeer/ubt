4c9c5a6f5e0d9e0ef3e00b8071fbe835b1872de6
Mahadeer Mohamed <mahadeer2010@gmail.com>
2019-12-12 23:32:03 +0530

    File IO Updated

--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,7 @@ export default {
   base: '/ubt',
   title: "Unified Build Tool",
   description: "Rust based Build Tool for Everything and Everyone",
-  menu: ['Introduction', 'Background and Concepts', 'Roadmap', 'Tutorials', 'Reference', 'Usage Policy', 'Contribute']
+  menu: ['Introduction', 'Background and Concepts', 'Roadmap', 
+  { name: 'Tutorials', menu: ['Hello World', 'File IO'] }, 
+  'Reference', 'Usage Policy', 'Contribute']
 }
