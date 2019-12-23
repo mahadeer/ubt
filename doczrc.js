b5dac4b59e7bf44b7ba9e0299aa639c8f2610e5f
mahadeer13 <mahadeer2010@gmail.com>
2019-12-23 15:44:41 +0530

    Contribute and Usage policy updated

--- doczrc.js
+++ doczrc.js
@@ -4,5 +4,5 @@ export default {
   description: "Rust based Build Tool for Everything and Everyone",
   menu: ['Introduction', 'Background and Concepts', 'Roadmap', 
   { name: 'Tutorials', menu: ['Hello World', 'File IO'] }, 
-  'Reference', 'Usage Policy', 'Contribute']
+  'Reference', 'Contribute', 'Usage Policy']
 }
