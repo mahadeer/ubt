c20fc78e28ad03c1854a9caa0ffa18e998ecfd31
Mahadeer Mohamed <mahadeer2010@gmail.com>
2020-01-19 00:18:05 +0530

    Updated to bypass build run

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-extern crate regex;
-extern crate roxmltree;
 extern crate fs_extra;
 extern crate glob;
+extern crate regex;
+extern crate roxmltree;
 
-mod logger;
-mod utils;
 mod builder;
+mod logger;
 mod tasks;
+mod utils;
 
 use std::env;
 
 fn main() {
-    let build_path: &str = &env::args().nth(1).expect("Build path xml must be provided");
+    let build_path: &str = &env::args().nth(1).unwrap_or("".to_string());
     let log = logger::Logger::new();
-    log.log(format!("\nBuildfile: {:?} \n", build_path));
-    utils::get_xml_doc(
-        std::path::Path::new(build_path).to_path_buf(),
-        &log,
-        |doc: roxmltree::Document| {
-            builder::build_project(&doc.root_element(), &log);
-        },
-    );
-    log.build_sucessful();
+    if build_path == "" {
+        log.log(format!("Error: Build xml path must be provided"));
+    } else {
+        log.log(format!("\nBuildfile: {:?} \n", build_path));
+        utils::get_xml_doc(
+            std::path::Path::new(build_path).to_path_buf(),
+            &log,
+            |doc: roxmltree::Document| {
+                builder::build_project(&doc.root_element(), &log);
+            },
+        );
+        log.build_sucessful();
+    }
 }
